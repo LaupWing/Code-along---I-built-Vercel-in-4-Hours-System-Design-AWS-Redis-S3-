52a7c047fe54d85fba266b2451cee2f53b70ada3
laupwing <laupwing@gmail.com>
2024-03-13 19:18:20 +0100

    Add POST endpoint for deployment

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,12 @@ import cors from "cors"
 const app = express()
 app.use(cors())
 
+app.post("/deploy", (req, res) => {
+    const rep_url = req.body.rep_url
+    console.log(rep_url)
+    res.json({})
+})
+
 app.listen(3000, () => {
     console.log("Server is running on port 3000")
 })
