c5c2dc25463392fdfb4a90f604cbe1b2cd2de2ab
laupwing <laupwing@gmail.com>
2024-03-18 19:18:41 +0100

    Add simple-git and generate function

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
 import express from "express"
+import simpleGit from "simple-git"
 import cors from "cors"
+import { generate } from "./utils"
 
 const app = express()
 app.use(express.json())
 app.use(cors())
 
-app.post("/deploy", (req, res) => {
+app.post("/deploy", async (req, res) => {
     const rep_url = req.body.rep_url
-    console.log(rep_url)
+    const id = generate()
+    await simpleGit().clone(rep_url, `output/${id}`)
     res.json({})
 })
 
